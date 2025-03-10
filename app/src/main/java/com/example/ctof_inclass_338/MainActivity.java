c3359241e116c77bb3768b22e944f93f19c3022f
Cooper_W_04 <cwestervelt@csumb.edu>
2025-03-10 11:38:44 -0700

    made what i could of when he lectured about

--- app/src/main/java/com/example/ctof_inclass_338/MainActivity.java
+++ app/src/main/java/com/example/ctof_inclass_338/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ctof_inclass_338;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +9,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+
+        setContentView(view);
     }
 }
